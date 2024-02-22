57bed6fe3249abd9fc9bb8a8ab1e5b290d556666
kondam <kondam@MAVCHN0522608.mavericsystems.lan>
2024-02-22 12:36:24 +0530

    commit01 by mahesh

---
+++ src/test/java/com/mybillbook/org/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybillbook.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
